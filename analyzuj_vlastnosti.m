bde785424b57bc6f26bab97bfaac594872a594d5
tempMatej <xkintler@stuba.sk>
2019-12-17 11:22:28 +0100

    uprava stability

--- analyzuj_vlastnosti.m
+++ analyzuj_vlastnosti.m
@@ -1,6 +1,7 @@
 function analyzuj_vlastnosti(cit,men)
     b = length(cit);
     a = length(men);
+    c = roots(men);
     
     if b < a 
         disp('System je striktne rydzi!')
@@ -9,4 +10,12 @@ function analyzuj_vlastnosti(cit,men)
     else
         disp('System nie je fyzikalne realizovatelny')
     end
+    
+    if sum(c > 0) == 0
+        disp('System je stabilny')
+    else
+        disp('System je nestabilny')
+    end
+        
+        
 end
